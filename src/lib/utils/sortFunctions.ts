d19f65989ae95d44c73404e5044c4c07b92745c8
Tony hart <tonyhart2020@gmail.com>
2023-06-29 05:27:01 +0700

    update sortfunction ts lint fix

--- src/lib/utils/sortFunctions.ts
+++ src/lib/utils/sortFunctions.ts
@@ -2,8 +2,8 @@
 export const sortByDate = (array: any[]) => {
   const sortedArray = array.sort(
     (a: any, b: any) =>
-      new Date(b.data.date && b.data.date) -
-      new Date(a.data.date && a.data.date)
+      new Date(b.data.date && b.data.date).getTime() -
+      new Date(a.data.date && a.data.date).getTime()
   );
   return sortedArray;
 };
